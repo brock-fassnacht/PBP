a49c66c742c9cba763b48f26ade36b728515cbcd
cchc-bfassnacht <136819738+cchc-bfassnacht@users.noreply.github.com>
2023-06-21 10:39:02 -0400

    Add files via upload

--- pbpdatapull.R
+++ pbpdatapull.R
@@ -8,7 +8,7 @@ library(dplyr)
 
 ## finding first and last gamepks of 2022 season
 
-opening_day <- as.Date("2022-04-07")
+opening_day <- as.Date("2022-10-28")
 closing_day <- as.Date("2022-11-05")
 
 game_pk_list_2022 <- list()
@@ -31,7 +31,7 @@ for (d in 1:length(date_range)){
 
 initial_df <- mlb_pbp(game_pk = game_pk_list_2022[1])
 
-needed_cols <- c("game_pk", "pitchNumber","result.rbi", "result.awayScore", "result.homeScore", "details.call.description", "count.strikes.x", "count.balls.x", "count.outs.x","last.pitch.of.ab", "matchup.splits.menOnBase", "pitchData.coordinates.x", "pitchData.coordinates.y", "hitData.hardness", "about.inning", "matchup.batSide.code", "matchup.pitchHand.code", "matchup.batter.id", "matchup.pitcher.id", "home_team", "away_team", "details.type.description", "pitchData.startSpeed", "pitchData.zone","pitchData.breaks.breakLength", "pitchData.breaks.spinRate", "details.isBall")
+needed_cols <- c("game_pk", "pitchNumber","result.rbi", "result.awayScore", "result.homeScore", "details.call.description", "count.strikes.x", "count.balls.x", "count.outs.x","last.pitch.of.ab", "matchup.splits.menOnBase", "pitchData.coordinates.x", "pitchData.coordinates.y", "hitData.hardness", "about.inning", "matchup.batSide.code", "matchup.pitchHand.code", "matchup.batter.id", "matchup.pitcher.id", "home_team", "away_team", "details.type.description", "pitchData.startSpeed", "pitchData.zone","pitchData.breaks.breakLength", "pitchData.breaks.spinRate", "details.isBall", "about.isTopInning", "matchup.batter.fullName")
 initial_df <- subset(initial_df, type=="pitch")[,needed_cols]
 
 miss_count = 0
@@ -54,10 +54,11 @@ for (x in 2:length(game_pk_list_2022)){
 
 copy1 <- initial_df
 
+# Making game pick an integer
 copy1$game_pk <- as.integer(unlist(copy1$game_pk))
 
 #Creating local copy of data
-write.csv(copy1, "C:\\Users\\bfass\\OneDrive\\Desktop\\CS\\Misc\\Baseball Data Bowl\\Pitchproj\\pbp2022.csv")
+write.csv(copy1, "C:\\Users\\BrocFassnacht\\OneDrive - Cobbs Creek Healthcare\\Desktop\\Training\\R Training\\GitR\\pbp2022.csv")
 
 
 
@@ -66,7 +67,7 @@ write.csv(copy1, "C:\\Users\\bfass\\OneDrive\\Desktop\\CS\\Misc\\Baseball Data B
 
 ## Strikezone Graph
 
-ggplot(inital_df, aes(pitchData.coordinates.x, pitchData.coordinates.y, color=factor(details.isBall))) + geom_point() +
+ggplot(copy1, aes(pitchData.coordinates.x, pitchData.coordinates.y, color=factor(details.isBall))) + geom_point() +
     labs(title = "Strikezone", x="X", y="Y") + scale_color_manual(values = c("red", "green"))
 
 
--- shiny_pbp.R
+++ shiny_pbp.R
@@ -13,12 +13,13 @@ library(DT)
 library(dplyr)
 library(data.table)
 
-pbp_data <- read.csv("C:\\Users\\bfass\\OneDrive\\Desktop\\CS\\Misc\\Baseball Data Bowl\\Pitchproj\\pbp2022.csv")
-
+pbp_data <- read.csv("C:\\Users\\BrocFassnacht\\OneDrive - Cobbs Creek Healthcare\\Desktop\\Training\\R Training\\GitR\\pbp2022.csv")
 last_p <- filter(pbp_data, last.pitch.of.ab == "true")
 
 last_p$hit <- ifelse(last_p$details.call.description == "Ball" | last_p$details.call.description == "Ball In Dirt" | last_p$details.call.description == "Hit By Pitch", 2, 
                      ifelse(last_p$details.call.description=="In play, no out" | last_p$details.call.description == "In play, run(s)", 1,0))
+
+last_p$hitting_team <- ifelse(last_p$about.isTopInning, last_p$away_team, last_p$home_team)
 
 stats_of_int <- c("BA", "BA vs RHP", "BA vs LHP", "BA w/ risp", "OBP")
 
@@ -29,16 +30,22 @@ ui <- fluidPage(
   # Create a new Row in the UI for selectInputs
   fluidRow(
     column(4,
+           selectInput("Home",
+                       "Hitting Team:",
+                       c("All",
+                         unique(as.character(last_p$hitting_team))))
+    ),
+    column(4,
+           selectInput("Batter",
+                       "Batter:",
+                       c("All",
+                         unique(as.character(last_p$matchup.batter.fullName))))
+    ),
+    column(4,
            selectInput("Pitch",
                        "Pitch:",
                        c("All",
                          unique(as.character(last_p$details.type.description))))
-    ),
-    column(4,
-           selectInput("Home",
-                       "Home_team:",
-                       c("All",
-                         unique(as.character(last_p$home_team))))
     )
   ),
   # Create a new row for the table.
@@ -50,13 +57,17 @@ server <- function(input, output) {
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
     filtered_data <- last_p
+    if (input$Home != "All") {
+      filtered_data <- filtered_data[filtered_data$hitting_team == input$Home,]
+    }
+    if (input$Batter != "All") {
+      filtered_data <- filtered_data[filtered_data$matchup.batter.fullName == input$Batter,]
+    }
     if (input$Pitch != "All") {
       filtered_data <- filtered_data[filtered_data$details.type.description == input$Pitch,]
     }
-    if (input$Home != "All") {
-      filtered_data <- filtered_data[filtered_data$home_team == input$Home,]
-    }
     
+    #team_player_list <- ifelse(input$Home != "All", unique(as.character(last_p[last_p$hitting_team == input$Home])),unique(as.character(last_p$matchup.batter.fullName)))
     
     summary_table <- data.table(
       Stat = c("BA", "BA vs. RHP", "BA vs. LHP", "BA w RISP", "OBP"),
@@ -67,11 +78,11 @@ server <- function(input, output) {
         sum(filtered_data[filtered_data$matchup.splits.menOnBase == "RISP" | filtered_data$matchup.splits.menOnBase == "Loaded",]$hit == 1)/sum(filtered_data[filtered_data$matchup.splits.menOnBase == "RISP" | filtered_data$matchup.splits.menOnBase == "Loaded",]$hit <= 1), # BA w RISP
         sum(filtered_data$hit != 0) / length(filtered_data$hit) # OBP
       ),
-      Obvs = c(
+      At_Bats = c(
         sum(filtered_data$hit <= 1),
         sum(filtered_data[filtered_data$matchup.pitchHand.code == "R",]$hit <= 1),
         sum(filtered_data[filtered_data$matchup.pitchHand.code == "L",]$hit <= 1),
-        sum(filtered_data[filtered_data$matchup.splits.menOnBase == "RISP",]$hit <= 1),
+        sum(filtered_data[filtered_data$matchup.splits.menOnBase == "RISP" | filtered_data$matchup.splits.menOnBase == "Loaded",]$hit <= 1),
         length(filtered_data$hit)
       )
     )
