f4d33de9c80dc52169a1f8ddb8a6145a35238526
Brock Fassnacht <96959426+brock-fassnacht@users.noreply.github.com>
2023-06-25 16:26:02 -0400

    Add files via upload

--- shiny_pbp.R
+++ shiny_pbp.R
@@ -12,10 +12,18 @@ library(ggplot2)
 library(DT)
 library(dplyr)
 library(data.table)
+library(lubridate)
 
 pbp_data <- read.csv("C:\\Users\\bfass\\OneDrive\\Desktop\\CS\\Misc\\Baseball Data Bowl\\Pitchproj\\pbp2022.csv")
 
+#extracting days since today
+pbp_data$game_date <- as.Date(pbp_data$game_date)
 
+pbp_data$monthh <- as.integer(format(pbp_data$game_date, "%m"))
+
+pbp_data$days_since_today <- today() - pbp_data$game_date
+
+# Getting hits and the team the batter is on
 pbp_data$hit <- ifelse(pbp_data$details.call.description == "Ball" | pbp_data$details.call.description == "Ball In Dirt" | pbp_data$details.call.description == "Hit By Pitch", 2, 
                      ifelse(pbp_data$details.call.description=="In play, no out" | pbp_data$details.call.description == "In play, run(s)", 1,0))
 
@@ -45,14 +53,22 @@ ui <- fluidPage(
     ),
     column(4,
            selectInput("Pitch",
-                       "Pitch:",
+                       "Pitch (last of at bat):",
                        c("All",
                          unique(as.character(last_p$details.type.description))))
     )
   ),
   # Create a new row for the table.
   DT::dataTableOutput("table"),
-  plotOutput("plott")
+  fluidRow(
+    column(width = 6,
+           plotOutput("plott")
+           ),
+    column(width = 6,
+           plotOutput("lineplot")
+           )
+  )
+  
 )
 
 server <- function(input, output, session) {
@@ -87,6 +103,13 @@ server <- function(input, output, session) {
     
     summary_table <- data.table(
       Stat = c("BA", "BA vs. RHP", "BA vs. LHP", "BA w RISP", "OBP"),
+      MLB_avg = c(
+        round(sum(last_p$hit == 1)/sum(last_p$hit <= 1),3), # BA
+        round(sum(last_p[last_p$matchup.pitchHand.code == "R",]$hit == 1)/sum(last_p[last_p$matchup.pitchHand.code == "R",]$hit <= 1),3), # BA vs. RHP
+        round(sum(last_p[last_p$matchup.pitchHand.code == "L",]$hit == 1)/sum(last_p[last_p$matchup.pitchHand.code == "L",]$hit <= 1),3), # BA vs. LHP
+        round(sum(last_p[last_p$matchup.splits.menOnBase == "RISP" | last_p$matchup.splits.menOnBase == "Loaded",]$hit == 1)/sum(last_p[last_p$matchup.splits.menOnBase == "RISP" | last_p$matchup.splits.menOnBase == "Loaded",]$hit <= 1),3), # BA w RISP
+        round(sum(last_p$hit != 0) / length(last_p$hit),3) # OBP
+      ),
       Avg. = c(
         round(sum(filtered_data$hit == 1)/sum(filtered_data$hit <= 1),3), # BA
         round(sum(filtered_data[filtered_data$matchup.pitchHand.code == "R",]$hit == 1)/sum(filtered_data[filtered_data$matchup.pitchHand.code == "R",]$hit <= 1),3), # BA vs. RHP
@@ -119,6 +142,52 @@ server <- function(input, output, session) {
       xlab("Pitch") + ylab("Number of Pitches") + labs(fill="Pitching Hand")
 
     })
+  
+  output$lineplot <- renderPlot({
+    
+    averages <- last_p[last_p$hit <= 1,] %>%
+      group_by(monthh, matchup.batter.fullName, hitting_team) %>%
+      summarize(total_hits=sum(hit), abs = n())
+    
+    leag_avgs <- averages %>%
+      group_by(monthh) %>%
+      summarize(total_hits=sum(total_hits), abs = sum(abs))
+    
+    leag_avgs$batting_avg <- leag_avgs$total_hits/leag_avgs$abs
+    
+    if (input$Batter != "All") {
+      averages <- averages[averages$matchup.batter.fullName == input$Batter,]
+      averages$batting_avg <- averages$total_hits/averages$abs
+      
+      ggplot(averages) + geom_line(aes(x = monthh, y=batting_avg, colour = "Selected Avg"), size=1) +
+        geom_line(data = leag_avgs, aes(x = monthh, y=batting_avg, colour = "League Avg"), size=1) +
+        labs(x = "Date", y = "Batting avg") + scale_color_manual(name = "Legend", values = c("Selected Avg" = "red", "League Avg" = "darkblue")) +
+        theme_minimal() +
+        scale_x_continuous(breaks = unique(averages$monthh))
+      
+    }else if (input$Home != "All") {
+      averages <- averages[averages$hitting_team == input$Home,]
+      
+      averages <- averages %>%
+        group_by(monthh, hitting_team) %>%
+        summarize(total_hits=sum(total_hits), abs = sum(abs))
+      
+      averages$batting_avg <- averages$total_hits/averages$abs
+      
+      ggplot(averages) + geom_line(aes(x = monthh, y=batting_avg, colour = "Selected Avg"), size=1) +
+        geom_line(data = leag_avgs, aes(x = monthh, y=batting_avg, colour = "League Avg"), size=1) +
+        labs(x = "Date", y = "Batting avg") + scale_color_manual(values = c("Selected Avg" = "red", "League Avg" = "darkblue")) +
+        theme_minimal() +
+        scale_x_continuous(breaks = unique(averages$monthh))
+      
+    } else {
+      
+      ggplot(leag_avgs) + geom_line(aes(x = monthh, y=batting_avg), color="blue") + 
+        labs(x = "Date", y = "Batting avg") + theme_minimal() +
+        scale_x_continuous(breaks = unique(averages$monthh))
+    }
+    
+  })
       }
 
 shinyApp(ui = ui, server = server)
@@ -144,3 +213,13 @@ last_p[matchup.pitchHand.code == "R",]
 View(last_p)
 
 runExample("01_hello")
+
+
+
+averages <- last_p %>%
+  group_by(monthh) %>%
+  summarize(total_hits=sum(hit), abs = n())
+
+averages$batting_avg <- averages$total_hits/averages$abs
+
+
