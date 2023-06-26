56bdb6e4c3fb547c7870a09138ec4b503b5ac381
Brock Fassnacht <96959426+brock-fassnacht@users.noreply.github.com>
2023-06-25 20:56:57 -0400

    Add files via upload

--- shiny_pbp.R
+++ shiny_pbp.R
@@ -7,6 +7,8 @@
 # Batting avg, batting avg vs lhp vs rhp, obp, w/ risp, Over season and last x amount of games
 # be able to filter by hitter(s) or pitcher
 
+# What pitches a batter swings and misses at, swings out of the zone for, hits the best etc.
+
 library(shiny)
 library(ggplot2)
 library(DT)
@@ -17,18 +19,22 @@ library(lubridate)
 pbp_data <- read.csv("C:\\Users\\bfass\\OneDrive\\Desktop\\CS\\Misc\\Baseball Data Bowl\\Pitchproj\\pbp2022.csv")
 
 #extracting days since today
-pbp_data$game_date <- as.Date(pbp_data$game_date)
+pbp_data$game_date <- as.Date(pbp_data$game_date) - 1
 
 pbp_data$monthh <- as.integer(format(pbp_data$game_date, "%m"))
 
-pbp_data$days_since_today <- today() - pbp_data$game_date
+opening_day <- min(pbp_data$game_date)
+closing_day <- max(pbp_data$game_date)
 
-# Getting hits and the team the batter is on
+pbp_data$week2 <- floor((pbp_data$game_date - opening_day)/14)
+
+# setting walks as 2, hits as 1 and outs as 0
 pbp_data$hit <- ifelse(pbp_data$details.call.description == "Ball" | pbp_data$details.call.description == "Ball In Dirt" | pbp_data$details.call.description == "Hit By Pitch", 2, 
                      ifelse(pbp_data$details.call.description=="In play, no out" | pbp_data$details.call.description == "In play, run(s)", 1,0))
 
 pbp_data$hitting_team <- ifelse(pbp_data$about.isTopInning, pbp_data$away_team, pbp_data$home_team)
 
+# dataframe where only last pitch of each at bat is captured (to calculate batting averages)
 last_p <- filter(pbp_data, last.pitch.of.ab == "true")
 
 stats_of_int <- c("BA", "BA vs RHP", "BA vs LHP", "BA w/ risp", "OBP")
@@ -39,27 +45,33 @@ ui <- fluidPage(
   
   # Create a new Row in the UI for selectInputs
   fluidRow(
-    column(4,
+    column(3,
            selectInput("Home",
                        "Hitting Team:",
                        c("All",
                          unique(as.character(last_p$hitting_team))))
     ),
-    column(4,
+    column(3,
            selectInput("Batter",
                        "Batter:",
                        c("All",
                          unique(as.character(last_p$matchup.batter.fullName))))
     ),
-    column(4,
+    column(3,
            selectInput("Pitch",
                        "Pitch (last of at bat):",
                        c("All",
                          unique(as.character(last_p$details.type.description))))
-    )
-  ),
-  # Create a new row for the table.
+    ),
+    column(3,
+           dateRangeInput("daterange",
+                          label = "Date range of interest:",
+                          start = min(last_p$game_date), max(last_p$game_date)
+           )
+  )),
+  # Create a new row for the table
   DT::dataTableOutput("table"),
+  # New row with both plots on same row
   fluidRow(
     column(width = 6,
            plotOutput("plott")
@@ -73,8 +85,10 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  #This changes the batters input based on the team selection to narrow down batter list
   observeEvent(input$Home, {
     home_team <- input$Home
+    last_p <- last_p[last_p$game_date >= input$daterange[1] & last_p$game_date <= input$daterange[2],]
     batters <- unique(as.character(last_p[last_p$hitting_team == home_team, "matchup.batter.fullName"]))
     if ("All" %in% batters) {
       batters <- setdiff(batters, "All")
@@ -82,10 +96,10 @@ server <- function(input, output, session) {
     updateSelectInput(session, "Batter", choices = c("All", batters))
   })
   
-  
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
-    filtered_data <- last_p
+    filtered_data <- last_p[last_p$game_date >= input$daterange[1] & last_p$game_date <= input$daterange[2],]
+    last_p2 <- last_p[last_p$game_date >= input$daterange[1] & last_p$game_date <= input$daterange[2],]
     if (input$Home != "All") {
       filtered_data <- filtered_data[filtered_data$hitting_team == input$Home,]
     }
@@ -96,19 +110,15 @@ server <- function(input, output, session) {
       filtered_data <- filtered_data[filtered_data$details.type.description == input$Pitch,]
     }
     
-    
-    
-    
-    
-    
+    #summary table based on filtered data from the inputs
     summary_table <- data.table(
       Stat = c("BA", "BA vs. RHP", "BA vs. LHP", "BA w RISP", "OBP"),
       MLB_avg = c(
-        round(sum(last_p$hit == 1)/sum(last_p$hit <= 1),3), # BA
-        round(sum(last_p[last_p$matchup.pitchHand.code == "R",]$hit == 1)/sum(last_p[last_p$matchup.pitchHand.code == "R",]$hit <= 1),3), # BA vs. RHP
-        round(sum(last_p[last_p$matchup.pitchHand.code == "L",]$hit == 1)/sum(last_p[last_p$matchup.pitchHand.code == "L",]$hit <= 1),3), # BA vs. LHP
-        round(sum(last_p[last_p$matchup.splits.menOnBase == "RISP" | last_p$matchup.splits.menOnBase == "Loaded",]$hit == 1)/sum(last_p[last_p$matchup.splits.menOnBase == "RISP" | last_p$matchup.splits.menOnBase == "Loaded",]$hit <= 1),3), # BA w RISP
-        round(sum(last_p$hit != 0) / length(last_p$hit),3) # OBP
+        round(sum(last_p2$hit == 1)/sum(last_p2$hit <= 1),3), # BA
+        round(sum(last_p2[last_p$matchup.pitchHand.code == "R",]$hit == 1)/sum(last_p2[last_p$matchup.pitchHand.code == "R",]$hit <= 1),3), # BA vs. RHP
+        round(sum(last_p2[last_p$matchup.pitchHand.code == "L",]$hit == 1)/sum(last_p2[last_p$matchup.pitchHand.code == "L",]$hit <= 1),3), # BA vs. LHP
+        round(sum(last_p2[last_p$matchup.splits.menOnBase == "RISP" | last_p2$matchup.splits.menOnBase == "Loaded",]$hit == 1)/sum(last_p2[last_p$matchup.splits.menOnBase == "RISP" | last_p2$matchup.splits.menOnBase == "Loaded",]$hit <= 1),3), # BA w RISP
+        round(sum(last_p2$hit != 0) / length(last_p2$hit),3) # OBP
       ),
       Avg. = c(
         round(sum(filtered_data$hit == 1)/sum(filtered_data$hit <= 1),3), # BA
@@ -129,8 +139,10 @@ server <- function(input, output, session) {
     
   }))
   
+  #Plot is a frequency plot of all the pitches a batter faces 
+  # note that we are not using last_p data frame
   output$plott <- renderPlot({
-    filtered_data2 <- pbp_data
+    filtered_data2 <- pbp_data[pbp_data$game_date >= input$daterange[1] & pbp_data$game_date <= input$daterange[2],]
     if (input$Home != "All") {
       filtered_data2 <- filtered_data2[filtered_data2$hitting_team == input$Home,]
     }
@@ -143,14 +155,14 @@ server <- function(input, output, session) {
 
     })
   
+  # plots batting average over course of the season. is grouped by every 10 days
   output$lineplot <- renderPlot({
-    
-    averages <- last_p[last_p$hit <= 1,] %>%
-      group_by(monthh, matchup.batter.fullName, hitting_team) %>%
+    averages <- last_p[last_p$game_date >= input$daterange[1] & last_p$game_date <= input$daterange[2] & last_p$hit <= 1,] %>%
+      group_by(week2, matchup.batter.fullName, hitting_team) %>%
       summarize(total_hits=sum(hit), abs = n())
     
     leag_avgs <- averages %>%
-      group_by(monthh) %>%
+      group_by(week2) %>%
       summarize(total_hits=sum(total_hits), abs = sum(abs))
     
     leag_avgs$batting_avg <- leag_avgs$total_hits/leag_avgs$abs
@@ -159,32 +171,32 @@ server <- function(input, output, session) {
       averages <- averages[averages$matchup.batter.fullName == input$Batter,]
       averages$batting_avg <- averages$total_hits/averages$abs
       
-      ggplot(averages) + geom_line(aes(x = monthh, y=batting_avg, colour = "Selected Avg"), size=1) +
-        geom_line(data = leag_avgs, aes(x = monthh, y=batting_avg, colour = "League Avg"), size=1) +
+      ggplot(averages) + geom_line(aes(x = week2, y=batting_avg, colour = "Selected Avg"), size=1) +
+        geom_line(data = leag_avgs, aes(x = week2, y=batting_avg, colour = "League Avg"), size=1) +
         labs(x = "Date", y = "Batting avg") + scale_color_manual(name = "Legend", values = c("Selected Avg" = "red", "League Avg" = "darkblue")) +
         theme_minimal() +
-        scale_x_continuous(breaks = unique(averages$monthh))
+        scale_x_continuous(breaks = unique(averages$week2))
       
     }else if (input$Home != "All") {
       averages <- averages[averages$hitting_team == input$Home,]
       
       averages <- averages %>%
-        group_by(monthh, hitting_team) %>%
+        group_by(week2, hitting_team) %>%
         summarize(total_hits=sum(total_hits), abs = sum(abs))
       
       averages$batting_avg <- averages$total_hits/averages$abs
       
-      ggplot(averages) + geom_line(aes(x = monthh, y=batting_avg, colour = "Selected Avg"), size=1) +
-        geom_line(data = leag_avgs, aes(x = monthh, y=batting_avg, colour = "League Avg"), size=1) +
+      ggplot(averages) + geom_line(aes(x = week2, y=batting_avg, colour = "Selected Avg"), size=1) +
+        geom_line(data = leag_avgs, aes(x = week2, y=batting_avg, colour = "League Avg"), size=1) +
         labs(x = "Date", y = "Batting avg") + scale_color_manual(values = c("Selected Avg" = "red", "League Avg" = "darkblue")) +
         theme_minimal() +
-        scale_x_continuous(breaks = unique(averages$monthh))
+        scale_x_continuous(breaks = unique(averages$week2))
       
     } else {
       
-      ggplot(leag_avgs) + geom_line(aes(x = monthh, y=batting_avg), color="blue") + 
+      ggplot(leag_avgs) + geom_line(aes(x = week2, y=batting_avg), color="blue") + 
         labs(x = "Date", y = "Batting avg") + theme_minimal() +
-        scale_x_continuous(breaks = unique(averages$monthh))
+        scale_x_continuous(breaks = unique(averages$week2))
     }
     
   })
@@ -217,7 +229,7 @@ runExample("01_hello")
 
 
 averages <- last_p %>%
-  group_by(monthh) %>%
+  group_by(game_pk, matchup.batter.fullName ,atBatIndex) %>%
   summarize(total_hits=sum(hit), abs = n())
 
 averages$batting_avg <- averages$total_hits/averages$abs
